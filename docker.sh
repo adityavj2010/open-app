d950b209fe0538aeea5320777bf431ad60ccb3b9
Aditya Jagtap <adityavj2010@gmail.com>
2022-04-16 18:42:12 -0400

    updated port

--- docker.sh
+++ docker.sh
@@ -3,8 +3,9 @@ docker rm openapp_server
 docker stop db
 docker rm db
 docker build . -t open-app-backend
-docker network rm -d bridge open-app
+docker network rm open-app
 docker network create -d bridge open-app
-docker run --detach --name db -e MYSQL_ROOT_PASSWORD="password" -e MYSQL_DATABASE="openapp"  -p 3306:3306 --network=open-app --platform linux/x86_64 mysql
+#docker run --detach --name db -e MYSQL_ROOT_PASSWORD="password" -e MYSQL_DATABASE="openapp"  -p 3306:3306 --network=open-app --platform linux/x86_64 mysql
+docker run --detach --name db -e MYSQL_ROOT_PASSWORD="password" -e MYSQL_DATABASE="openapp"  -p 3306:3306 --network=open-app mysql
 sleep 5
 docker run --detach --name openapp_server -p 5001:3000 --network=open-app -t open-app-backend npm start
