c99e38dfc000a7b28a156780e6794f3450f1c2c5
Aditya Jagtap <adityavj2010@gmail.com>
2022-05-11 16:09:19 -0400

    Updated scripts

--- update-backend.sh
+++ update-backend.sh
@@ -1,4 +1,5 @@
 git pull --recurse-submodules
 docker stop openapp_server
 docker rm openapp_server
+docker build . -t open-app-backend
 docker run --detach --name openapp_server -p 5001:3000 --network=open-app -t open-app-backend npm start
